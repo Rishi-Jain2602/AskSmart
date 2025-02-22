d3322c2182c2de2428f0d46cc3a1535f08cd95fa
Rishi-Jain2602 <rj1016743@gmail.com>
2025-02-22 22:03:57 +0530

    Adding routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Upload from './components/Upload';
+import Home from './components/Home';
+import Chat from './components/Chat'
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <div className="container">
-        Hi RIshi Jain
-      </div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/upload' element={<Upload/>} />
+          <Route path='/upload' element={<Upload/>} />
+          <Route path='/chat' element={<Chat/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
