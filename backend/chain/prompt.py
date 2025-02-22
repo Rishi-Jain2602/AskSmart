6e0bf3d7fd7db974eac5b910b9b073d004e29e6c
Rishi-Jain2602 <rj1016743@gmail.com>
2025-02-23 00:24:54 +0530

    Upd Prompt

--- backend/chain/prompt.py
+++ backend/chain/prompt.py
@@ -2,7 +2,12 @@ from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 
 Query_Prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a highly reliable and precise assistant for a RAG application. Your task is to answer user questions strictly using the provided DOCUMENT:{context} and any relevant conversation HISTORY :{history}. Ensure your response is concise, factual, and based only on these inputs."),
+        ("system", """You are a highly reliable and precise assistant for a RAG application. Your task is to answer user questions strictly using the provided DOCUMENT:{context} and any relevant conversation HISTORY :{history}. Ensure your response is concise, factual, and based only on these inputs.
+         Context: {context}
+
+         If context provided to you is insufficient then reply to user query i.e. {query} by saying "Apologies, but this query falls beyond my training data. Let me know if I can assist you with anything else!"
+         
+         """),
         ("human","{query}")
     ]
 )
